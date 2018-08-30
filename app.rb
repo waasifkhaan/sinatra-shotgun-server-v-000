26e95d2ee2e62d936210953d428f16ecd798e8c9
Wasif Khan <waasif.khaan@gmail.com>
2018-08-30 22:33:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! I was kidding 
+    <br>
+    started using shotgun gem "
   end
 
 end
